e982a40837a074a6589853731113080162f4b94f
Moracha <jacobmoracha@gmail.com>
2021-03-01 16:00:18 +0300

    added skills ,teams and contact section

--- script.js
+++ script.js
@@ -1,13 +1,34 @@
- $(document).ready(function(){
-	$(window).scroll(function(){
-		if(this.scrollY > 20){
+$(document).ready(function () {
+	$(window).scroll(function () {
+		if (this.scrollY > 20) {
 			$('.navbar').addClass("sticky");
-		}else{
-				$('.navbar').removeClass("sticky");
+		} else {
+			$('.navbar').removeClass("sticky");
 		}
 	});
-	$('.menu-btn').click(function(){
+	$('.menu-btn').click(function () {
 		$('.navbar .menu').toggleClass("active");
 		$('.menu-btn i').toggleClass("active");
 	});
- }); 
+	// owl carousel styling
+	$('.carousel').owlCarousel({
+		margin: 20,
+		loop: true,
+		autoPlayTimeout: 2000,
+		autoPlayHoverPause: true,
+		responsive: {
+			0: {
+				items: 1,
+				nav: false
+			},
+			600: {
+				items: 2,
+				nav: false
+			},
+			1000: {
+				items: 3,
+				nav: false
+			}
+		}
+	});
+}); 
